664f273c3d50a8833c9b1aa33c01da64010f6db0
Andrea Bentley <andrea@bentleytech.com>
2020-03-21 13:00:57 -0400

    changed character arrays to global constants

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+// Create constant arrays for the four different types of characters
+const lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialArray = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?'];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // This function handles the prompts and validation for the password length
 function getPasswordLength() {
   var isValidLength = false; // Used to determine if user selected valid length
   var pwdLength = 0; // Initialize return value
-  
+
   // This loop gets user input on password length and validates it
   while (!isValidLength) {
-    pwdLength = prompt("How many characters would you like in your password (8-12)?");
+    pwdLength = prompt("How many characters would you like in your password (8-128)?");
 
     // If user presses cancel, end function and return 0
     if (!pwdLength) {
       return 0;
-    } 
+    }
     // otherwise, convert input to a number
     else {
       pwdLength = parseInt(pwdLength);
     }
-    
+
     // Have to verify that it is a number and is between 8 and 128, inclusive
     isValidLength = (((typeof pwdLength) == "number") && (pwdLength >= 8) && (pwdLength <= 128));
 
@@ -28,34 +35,34 @@ function getPasswordLength() {
       if (!confirm("That is not a valid password length. Continue?")) {
         return 0;
       }
-    } 
+    }
   } // while !isValidLength
 
   return pwdLength;
 } // function getPasswordLength
 
-function createPassword(pwdLength,hasLower,hasUpper,hasNumeric,hasSpecial) {
+function createPassword(pwdLength, hasLower, hasUpper, hasNumeric, hasSpecial) {
   var newPwd = ''; // Initialize return value
   var charArray = []; // Initialize list of possible characters
 
   // If user selected lowercase, add all of the letters
   if (hasLower) {
-    charArray.push('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    charArray = charArray.concat(lowerArray);
   }
 
   // If user selected uppercase, add all of the letters
   if (hasUpper) {
-    charArray.push('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+    charArray = charArray.concat(upperArray);
   }
 
   // If user selected numbers, add all digits
   if (hasNumeric) {
-    charArray.push('0','1','2','3','4','5','6','7','8','9');
+    charArray = charArray.concat(numberArray);
   }
 
   // If user selected special characters, add them
   if (hasSpecial) {
-    charArray.push('`','~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[','{',']','}','\\','|',';',':',"'",'"',',','<','.','>','/','?');
+    charArray = charArray.concat(specialArray);
   }
 
   // Add the specified number of characters to the password
@@ -71,11 +78,10 @@ function createPassword(pwdLength,hasLower,hasUpper,hasNumeric,hasSpecial) {
 } // function createPassword
 
 function generatePassword() {
-  var newPwd = ''; // This will be the value that the function returns
   var hasSelection = false; // Used to ensure user selected at least one character type
-  
+
   var pwdLength = getPasswordLength(); // Prompt user for password length
-  
+
   // if pwdLength = 0 then the user quit without enter a valid length
   if (pwdLength !== 0) {
     // This loop gets user input on types of characters and validates
@@ -84,27 +90,22 @@ function generatePassword() {
       var hasUpper = confirm("Woud you like to include uppercase characters?");
       var hasNumeric = confirm("Would you like to include numbers?");
       var hasSpecial = confirm("Would you like to include special characters?");
-    
+
       hasSelection = (hasLower || hasUpper || hasNumeric || hasSpecial);
-    
+
       // If user has not selected at least one type, confirm that they want to continue
-      if ((!hasSelection) && (!confirm("You must have at least one type of character. Continue?"))) { 
-        return newPwd;
+      if ((!hasSelection) && (!confirm("You must have at least one type of character. Continue?"))) {
+        return '';
       }
     } // while !hasSelection
   } // if pwdLenth <> 0
-  
-  newPwd = createPassword(pwdLength,hasLower,hasUpper,hasNumeric,hasSpecial);
-  
-  return newPwd;
+
+  return createPassword(pwdLength, hasLower, hasUpper, hasNumeric, hasSpecial);
 } // function generatePassword
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.value = generatePassword();
 }
 
 // Add event listener to generate button
